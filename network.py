eb68b06d4458b771db2e3f2b25d56ebf4723f742
Innoviox <sarras305@gmail.com>
2018-06-12 09:33:24 -0400

    cleaned up code a little

--- network.py
+++ network.py
@@ -34,21 +34,22 @@ def neural(data):
     output= tf.matmul(l3, output_layer['weights'])+ output_layer['biases']
     return output
 
+prediction = neural(x)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction,labels=y))
+optimizer = tf.train.AdamOptimizer().minimize(cost)
+hm_epochs = 10
+    
 def rgb_to_greyscale(pixels):
     r, g, b, a = pixels
     return .2126 * (r / 255) + .7152 * (g / 255) + 0.0722 * (b/255)
 
 def train(x):
-    prediction = neural(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction,labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
-    hm_epochs = 10
     iv = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(iv)
         saver = tf.train.Saver()
         for epoch in range(hm_epochs):
-            epoch_loss=0
+            epoch_loss = 0
             for _ in range(int(mnist.train.num_examples / batch_size)):
                 epoch_x, epoch_y = mnist.train.next_batch(batch_size)
                 _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
@@ -56,14 +57,10 @@ def train(x):
             print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:',epoch_loss)
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
         saver.save(sess, './checkpoint/mnist.ckpt')
 
 def test():
-    prediction = neural(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction,labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
-    hm_epochs = 10
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
@@ -72,10 +69,10 @@ def test():
             img = Image.open("test/{}.png".format(i))
             features = np.array(list(map(rgb_to_greyscale, img.getdata())))
 
-            result = (sess.run(tf.argmax(prediction.eval(feed_dict={x:[features]}),1)))
+            result = sess.run(tf.argmax(prediction.eval(feed_dict={x: [features]}), 1))
             print(i, result[0])
             if i != result[0]:
                img.show()
 
-#train(x)
+train(x)
 test()
