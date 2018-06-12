b3bdb56771485aa96664a59bd4890953d6222386
Innoviox <sarras305@gmail.com>
2018-06-12 10:36:30 -0400

    Shows accuracy every iteration

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 n_nodes = [500, 500, 500]
 in_classes = 784
-n_classes = 10
+out_classes = 10
 batch_size = 100
 n_epochs = 10
 x = tf.placeholder('float', [None, in_classes])
@@ -16,7 +16,9 @@ r = lambda *d: tf.Variable(tf.random_normal(d))
 
 def neural(data, n_nodes, f=tf.nn.relu):
     layers = [{'weights': r(a, b), 'biases': r(b), 'f': c} for a, b, c in
-              zip([in_classes] + n_nodes, n_nodes + [n_classes], [f for _ in n_nodes] + [lambda _:_])]
+              zip([in_classes]+n_nodes,
+                  n_nodes+[out_classes],
+                  [f for _ in n_nodes]+[lambda _:_])]
     calc = data
     for l in layers:
         calc = l['f'](tf.add(tf.matmul(calc, l['weights']), l['biases']))
@@ -43,9 +45,9 @@ def train(x):
                 _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
             print('Epoch', epoch, 'completed out of', n_epochs, 'loss:',epoch_loss)
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-        print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+            print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
         saver.save(sess, './checkpoint/mnist.ckpt')
 
 def test():
